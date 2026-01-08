025c7e58febd52050f7bae2cd96f990ccb3ea440
BenjaSorkin <bs4171@princeton.edu>
2026-01-08 01:44:35 -0500

    changed sqrt to ^1/2 to be less bulky

--- Fig3b.py
+++ Fig3b.py
@@ -217,7 +217,7 @@ def plot_Tc_vs_P_scatter(df: pd.DataFrame) -> Tuple[float, float]:
         print(rf"  R^{2.0}     = {R2:.6f}")
 
     ax.set_xlabel(r"$P/P_0$")
-    ax.set_ylabel(r"$T_\mathrm{c}\left(1 + 1/\sqrt{N}\right)^2/(z\bar{\epsilon})$")
+    ax.set_ylabel(r"$T_\mathrm{c}\left(1 + 1/N^{1/2}\right)^2/(z\bar{\epsilon})$")
     ax.grid(False)
 
     if len(unique_N) <= 6:
@@ -269,7 +269,7 @@ def plot_Tc_vs_P_smoothed_density_contourf(
     ax.plot(xs, fit_slope * xs + fit_intercept, color="crimson", lw=1.4)
 
     ax.set_xlabel(r"$P/P_0$")
-    ax.set_ylabel(r"$T_\mathrm{c}\left(1 + 1/\sqrt{N}\right)^2/(z\bar{\epsilon})$")
+    ax.set_ylabel(r"$T_\mathrm{c}\left(1 + 1/N^{1/2}\right)^2/(z\bar{\epsilon})$")
     ax.grid(False)
 
     fig.tight_layout()
